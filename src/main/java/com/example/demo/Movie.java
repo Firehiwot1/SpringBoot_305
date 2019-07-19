f1184cae2672e96e79714adbbb3a41c90ceac54d
Firehiwot1 <fmazengia@gmail.com>
2019-07-19 16:46:50 -0400

    first initial

---
+++ src/main/java/com/example/demo/Movie.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private long year;
+    private String description;
+
+    @ManyToMany
+   private Set<Movie> Movie;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getYear() {
+        return year;
+    }
+
+    public void setYear(long year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<com.example.demo.Movie> getMovie() {
+        return Movie;
+    }
+
+    public void setMovie(Set<com.example.demo.Movie> movie) {
+        Movie = movie;
+    }
+}
